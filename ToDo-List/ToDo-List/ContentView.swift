f9248f4f5ae9e4de44a6fa7fa1aaf3352822bb3d
Savva Ponomarev <s.ponomarev@innopolis.university>
2025-07-31 18:50:36 +0300

    project init

---
+++ ToDo-List/ToDo-List/ContentView.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView.swift
+//  ToDo-List
+//
+//  Created by Савва Пономарев on 31.07.2025.
+//
+
+import SwiftUI
---
+++ ToDo-List/ToDo-List/ToDo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ListApp.swift
+//  ToDo-List
+//
+//  Created by Савва Пономарев on 31.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
